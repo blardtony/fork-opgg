8ef9a2ea7acaacb79ae6e208759eda7196984c26
karimsalhi <karimsalhi.contact@gmail.com>
2022-12-14 17:03:59 +0100

    Insert summoner in db

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Use env
-require('dotenv').config();
+require("dotenv").config();
 
 // Use Express
 const express = require("express");
 
 // Use node-fetch
 const fetch = (...args) =>
-  import('node-fetch').then(({default: fetch}) => fetch(...args));
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 // Create new instance of the express server
 const app = express();
@@ -18,25 +18,26 @@ const port = 3000;
 const mongoose = require("mongoose");
 
 // Connect to mongoDB
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
-mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.URL_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('error', (error) => console.error(error));
+db.on("error", (error) => console.error(error));
 
-db.once('open', () => console.log('Connected to database'))
-
+db.once("open", () => console.log("Connected to database"));
 
 const getStatus = (req, res) => {
   res.status(200).json("Status up");
-}
+};
 /**
  * GET: Get server status
  */
 app.get("/api/status", getStatus);
-
 
 /**
  * Create Schema Summoner for MongoDb
@@ -44,52 +45,65 @@ app.get("/api/status", getStatus);
 const schemaSummoner = new mongoose.Schema({
   id: {
     type: String,
-    required: true
+    required: true,
   },
   accountId: {
     type: String,
-    required: true
+    required: true,
   },
   name: {
     type: String,
-    required: true
+    required: true,
   },
   puuid: {
     type: String,
-    required: true
+    required: true,
   },
   summonerLevel: {
     type: Number,
-    required: true
+    required: true,
   },
   revisionDate: {
     type: Number,
-    required: true
+    required: true,
   },
   profileIconId: {
     type: Number,
-    required: true
-  }
+    required: true,
+  },
 });
 
+const modelSummoner = mongoose.model("Summoner", schemaSummoner, "Summoner");
 
-const modelSummoner = mongoose.model('Summoner', schemaSummoner);
-
-async function getSummonerByName (req, res) {
+async function getSummonerByName(req, res) {
   try {
-    const name = req.params.name;
-    const summoner = await modelSummoner.findOne({name: name});
-    if (null === summoner) {
+    const name = req.params.name.toLowerCase();
+    let summoner = null;
+    await modelSummoner.collection.findOne({ name: name }).then((res) => {
+      summoner = res;
+    });
+    if (summoner == null) {
       try {
-        const resRiot = await fetch(process.env.URL_RIOT_SUOMMNER_BY_NAME + name + '?api_key=' + process.env.API_KEY_RIOT)
+        const resRiot = await fetch(
+          process.env.URL_RIOT_SUOMMNER_BY_NAME +
+            name +
+            "?api_key=" +
+            process.env.API_KEY_RIOT
+        );
         const jsonRiot = await resRiot.json();
-        res.status(200).json(jsonRiot)
-      }catch (err) {
-        console.error(err.message)
+        jsonRiot.name = jsonRiot.name.toLowerCase();
+        res.status(200).json(jsonRiot);
+        modelSummoner.collection.insertOne(jsonRiot, (res, err) => {
+          console.log("Inserted")
+        });
+      } catch (err) {
+        console.error(err.message);
       }
+    } else {
+      res.status(200).json(summoner);
     }
   } catch (err) {
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message });
   }
 }
 app.get("/api/summoner/:name", getSummonerByName);
@@ -99,4 +113,3 @@ app.listen(port, () => {
 });
 
 app.use(express.json());
-
