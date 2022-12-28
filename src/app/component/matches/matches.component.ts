aa0ee6d6b1af8a866f8e956de76b7a172c1ab094
Tony Blard <tonyblard55@gmail.com>
2022-12-28 20:47:09 +0100

    Order by date ASC or DESC

--- src/app/component/matches/matches.component.ts
+++ src/app/component/matches/matches.component.ts
@@ -7,15 +7,30 @@ import {Match} from "../../models/match.model";
   styleUrls: ['./matches.component.scss']
 })
 export class MatchesComponent implements OnInit {
-
   constructor() {
     this.matches = []
+    this.filteredMatches = []
+    this.asc = false
   }
   @Input()
   matches: Array<Match>;
+  filteredMatches: Array<Match>
+  asc: boolean
   ngOnInit(): void {
+    this.filteredMatches = this.matches
   }
 
 
-
+  orderByDate(asc: boolean) {
+    console.log("OrderBy")
+    this.asc = !asc
+    if (asc) {
+      return this.filteredMatches.sort((a, b) => {
+        return new Date(a.gameCreation) < new Date(b.gameCreation) ? 1 : new Date(a.gameCreation) > new Date(b.gameCreation) ? -1 : 0
+      })
+    }
+    return this.filteredMatches.sort((a, b) => {
+      return new Date(a.gameCreation) > new Date(b.gameCreation) ? 1 : new Date(a.gameCreation) < new Date(b.gameCreation) ? -1 : 0
+    })
+  }
 }
