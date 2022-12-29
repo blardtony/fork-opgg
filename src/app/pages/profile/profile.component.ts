879531c0213c5aa6319b31a079555a9505a3fff3
Tony Blard <tonyblard55@gmail.com>
2022-12-29 20:57:01 +0100

    Profile card done

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -23,7 +23,7 @@ export class ProfileComponent implements OnInit {
   constructor(private summonerService : SummonerService, private route: ActivatedRoute) {
     this.rankTier = "";
     this.lp = 0;
-    this.imgPath="https://opgg-static.akamaized.net/images/medals_new/silver.png?image=q_auto,f_webp,w_144&v=1670665277508";
+    this.imgPath=""
     this.matchesId = []
     this.matches = []
     this.summoner = null;
@@ -41,7 +41,8 @@ export class ProfileComponent implements OnInit {
     )
       .subscribe(summoner => {
       this.summoner = summoner
-      this.rankTier = this.summoner.tier + " " + this.summoner.rank
+      this.rankTier = this.summoner.tier + " " + this.summoner.rank;
+      this.imgPath = this.getImg(this.summoner?.tier)
       this.lp = this.summoner.leaguePoints
       this.summonerService.getAllMatchsId(this.summoner.puuid).subscribe(matches => {
         this.matchesId = matches;
@@ -52,6 +53,44 @@ export class ProfileComponent implements OnInit {
         })
       })
     })
+  }
+
+  getImg(tier:string): string {
+    if (tier === "GOLD") {
+      return "https://opgg-static.akamaized.net/images/medals_new/gold.png?image=q_auto,f_webp,w_144&v=1672331410427"
+    }
+
+    if (tier === "SILVER") {
+      return "https://opgg-static.akamaized.net/images/medals_new/silver.png?image=q_auto,f_webp,w_144&v=1670665277508";
+    }
+
+    if (tier === "DIAMOND") {
+      return "https://opgg-static.akamaized.net/images/medals_new/diamond.png?image=q_auto,f_webp,w_144&v=1672331410233"
+    }
+    if (tier === "PLATINUM") {
+      return "https://opgg-static.akamaized.net/images/medals_new/platinum.png?image=q_auto,f_webp,w_144&v=1672331410233"
+    }
+
+    if (tier === "MASTER") {
+      return "https://opgg-static.akamaized.net/images/medals_new/master.png?image=q_auto,f_webp,w_144&v=1672331410233"
+    }
+
+    if (tier === "IRON") {
+      return "https://opgg-static.akamaized.net/images/medals_new/iron.png?image=q_auto,f_webp,w_144&v=1672331410233"
+    }
+
+    if (tier === "BRONZE") {
+      return "https://opgg-static.akamaized.net/images/medals_new/bronze.png?image=q_auto,f_webp,w_144&v=1672331410233"
+    }
+
+    if (tier === "CHALLENGER") {
+      return "https://opgg-static.akamaized.net/images/medals_new/challenger.png?image=q_auto,f_webp,w_144&v=1672331410427"
+    }
+
+    if (tier === "GRANDMASTER") {
+      return "https://opgg-static.akamaized.net/images/medals_new/grandmaster.png?image=q_auto,f_webp,w_144&v=1672331410427"
+    }
+    return ""
   }
 
 }
